a7209f21ae04c46f2146ed735ef76763342b9f02
Ariel Malka <ariel@chronotext.org>
2015-07-21 03:13:46 +0300

    STARTING WITH deps/protobuf

---
+++ deps/protobuf/build.mxe.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+PLATFORM="mxe"
+
+SRC_DIR="build/src"
+BUILD_DIR="build/$PLATFORM"
+INSTALL_DIR="dist/$PLATFORM"
+
+SRC_PATH="$(pwd)/$SRC_DIR"
+INSTALL_PATH="$(pwd)/$INSTALL_DIR"
+
+if [ ! -d "$SRC_PATH" ]; then
+  echo "SOURCE NOT FOUND!"
+  exit 1
+fi
+
+# ---
+
+TOOLCHAIN_FILE="$CROSS_PATH/core/cmake/toolchains/mxe.cmake"
+
+cmake -H"$SRC_DIR/cmake" -B"$BUILD_DIR" \
+  -DCMAKE_TOOLCHAIN_FILE="$TOOLCHAIN_FILE" -G Ninja \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DLIBRARY_OUTPUT_PATH="$INSTALL_PATH/lib" \
+  -DZLIB=ON
+
+if [ $? != 0 ]; then
+  echo "CONFIGURATION FAILED!"
+  exit 1
+fi
+
+# ---
+
+rm -rf "$INSTALL_PATH" # XXX: REQUIRED?
+cmake --build "$BUILD_DIR"
+
+if [ $? != 0 ]; then
+  echo "BUILD FAILED!"
+  exit 1
+fi
+
+cd "$INSTALL_PATH"
+ln -s "$SRC_PATH/src" include
