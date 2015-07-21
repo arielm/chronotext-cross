70bcdc66a4fcd5cb9bf8de3f31ff4e591ecd26f3
Ariel Malka <ariel@chronotext.org>
2015-07-21 04:31:40 +0300

    STARTING WITH tests/TestingProtobuf

---
+++ tests/TestingProtobuf/CTestConfig.cmake
@@ -0,0 +1,39 @@
+
+set(CTEST_PROJECT_NAME TestingProtobuf)
+set(CTEST_CONFIGURATION_TYPE Release)
+
+if (PLATFORM MATCHES osx)
+  set(ARGS
+    -DRUN=EXE
+  )
+
+elseif (PLATFORM MATCHES ios)
+  set(ARGS
+    -DRUN=APP
+#   -DIOS_DEPLOYMENT_TARGET=5.1.1
+#   -DIOS_ARCHS=armv7
+  )
+
+elseif (PLATFORM MATCHES android)
+  set(ARGS
+    -DRUN=EXE
+  )
+
+elseif (PLATFORM MATCHES emscripten)
+  set(ARGS
+    -DRUN=NODE
+  )
+
+elseif (PLATFORM MATCHES mxe)
+  set(ARGS
+    -DRUN=EXE
+  )
+endif()
+
+list(APPEND ARGS
+  "-DBOOST_ROOT=$ENV{CROSS_PATH}/deps/boost/dist/${PLATFORM}"
+  "-DGTEST_ROOT=$ENV{CROSS_PATH}/deps/gtest/dist/${PLATFORM}"
+  "-DPROTOBUF_ROOT=$ENV{CROSS_PATH}/deps/protobuf/dist/${PLATFORM}"
+)
+
+include("${CROSS_ROOT}/cmake/platforms.cmake")
