510988d59c36c5f36b7b31b794ef716807a090d1
Ariel Malka <ariel@chronotext.org>
2015-07-21 19:29:56 +0300

    TestingFileSystem3: PERFORMANCE GAIN...
    ... CREATING A CodedInputStream "DIRECTLY" FROM MEMORY-BUFFER...

--- tests/TestingFileSystem3/src/main.cpp
+++ tests/TestingFileSystem3/src/main.cpp
@@ -10,13 +10,14 @@
 #include <fcntl.h> // FOR MINGW + O_RDONLY
 
 using namespace std;
-using namespace google::protobuf::io;
+using namespace google::protobuf;
 
 TEST(TestFileSystem3, ZeroCopyInputStream)
 {
   fs::path path = "Georgia_Regular_64.fnt";
 
-  ZeroCopyInputStream *rawInput = nullptr;
+  io::CodedInputStream *codedInput = nullptr;
+  io::ZeroCopyInputStream *rawInput = nullptr;
   shared_ptr<chr::MemoryBuffer> memoryBuffer;
   int fd = 0;
 
@@ -26,7 +27,7 @@ TEST(TestFileSystem3, ZeroCopyInputStream)
 
     if (memoryBuffer)
     {
-      rawInput = new ArrayInputStream(memoryBuffer->data(), memoryBuffer->size());
+      codedInput = new io::CodedInputStream(reinterpret_cast<const uint8*>(memoryBuffer->data()), memoryBuffer->size());
     }
     else
     {
@@ -40,7 +41,8 @@ TEST(TestFileSystem3, ZeroCopyInputStream)
 
     if (fd > 0)
     {
-      rawInput = new FileInputStream(fd);
+      rawInput = new io::FileInputStream(fd);
+      codedInput = new io::CodedInputStream(rawInput);
     }
     else
     {
@@ -48,20 +50,19 @@ TEST(TestFileSystem3, ZeroCopyInputStream)
     }
   }
 
-  if (rawInput)
+  if (codedInput)
   {
-    CodedInputStream *codedInput = new CodedInputStream(rawInput);
-
     string version;
+
     if (codedInput->ReadString(&version, 9))
     {
       LOGI << "{" << version << "}" << endl;
 
       delete codedInput;
-      delete rawInput;
 
-      if (fd)
+      if (rawInput)
       {
+        delete rawInput;
         close(fd);  
       }
     }
