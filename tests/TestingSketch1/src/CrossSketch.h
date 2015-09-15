49a48e556e284dbc11bbedadedfc7819154ea2cf
Ariel Malka <ariel@chronotext.org>
2015-09-15 18:12:32 +0300

    TestingSketch1: TEXTURE-RENDERING IN PLACE (WORKS PARTIALLY...)
    FIXME:
    - TEXTURING AND MIPMAP ARE BROKEN ON DESKTOP
    
    TODO:
    - ANISOTROPIC FILTERING

--- tests/TestingSketch1/src/CrossSketch.h
+++ tests/TestingSketch1/src/CrossSketch.h
@@ -2,6 +2,7 @@
 
 #include "Log.h"
 #include "Platform.h"
+#include "MemoryBuffer.h"
 
 #if defined(CHR_PLATFORM_DESKTOP)
   #include <glad/glad.h>
@@ -17,12 +18,16 @@
 #include <glm/gtc/constants.hpp>
 #include <glm/gtc/epsilon.hpp>
 
+// ---
+
 #ifndef M_PI
   #define M_PI 3.14159265358979323846
 #endif
 
 static constexpr float D2R (M_PI / 180.0);
 static constexpr float R2D (180.0 / M_PI);
+
+// ---
 
 namespace chr
 {
--- tests/TestingSketch1/src/main.cpp
+++ tests/TestingSketch1/src/main.cpp
@@ -4,17 +4,27 @@
 using namespace std;
 using namespace chr;
 
+#define STB_IMAGE_IMPLEMENTATION
+#define STBI_ONLY_JPEG
+#define STBI_ONLY_PNG
+
+#include "stb_image.h"
+
 // ---
 
 static const char *vss = R"(
 attribute vec2 a_position;
+attribute vec2 a_coord;
 attribute vec4 a_color;
 
 uniform mat4 u_mvp_matrix;
+
+varying vec2 v_coord;
 varying vec4 v_color;
 
 void main()
 {
+  v_coord = a_coord;
   v_color = a_color;
   gl_Position = u_mvp_matrix * vec4(a_position, 0, 1);
 }
@@ -25,11 +35,14 @@ static const char *pss = R"(
   precision mediump float;
 #endif
 
+uniform sampler2D u_sampler;
+
+varying vec2 v_coord;
 varying vec4 v_color;
 
 void main()
 {
-  gl_FragColor = v_color;
+  gl_FragColor = v_color * texture2D(u_sampler, v_coord);
 }
 )";
 
@@ -42,32 +55,66 @@ public:
     void shutdown() final;
     void draw() final;
 
+    void initBuffers();
+    void initTextures();
     void initShaders();
-    void createBuffers();
 
 protected:
     GLuint shaderProgram;
     GLint positionLocation;
+    GLint coordLocation;
     GLint colorLocation;
+    GLint samplerLocation;
     GLint matrixLocation;
-    GLuint vboIds[2];
+
+    GLuint vboIds[3];
+    GLuint textureIds[1];
 };
 
 void Sketch::setup()
 {
-    createBuffers();
+    initBuffers();
+    initTextures();
     initShaders();
+
+    // ---
+
+    glEnable(GL_TEXTURE_2D);
+
+    glDisable(GL_DEPTH_TEST);
+    glDepthMask(GL_FALSE);
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 }
 
 void Sketch::shutdown()
 {
     glUseProgram(0);
     glDisableVertexAttribArray(positionLocation);
-    glDeleteBuffers(2, vboIds);
+    glDisableVertexAttribArray(coordLocation);
+    glDeleteBuffers(3, vboIds);
+    glDeleteTextures(1, textureIds);
 }
 
 void Sketch::draw()
 {
+    glEnableVertexAttribArray(positionLocation);
+    glBindBuffer(GL_ARRAY_BUFFER, vboIds[0]);
+    glVertexAttribPointer(positionLocation, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
+    glEnableVertexAttribArray(coordLocation);
+    glBindBuffer(GL_ARRAY_BUFFER, vboIds[1]);
+    glVertexAttribPointer(coordLocation, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
+    GLfloat color[4] = {1.0f, 1.0f, 1.0f, 1.0f};
+    glVertexAttrib4fv(colorLocation, color);
+
+    glActiveTexture(GL_TEXTURE0);
+    glUniform1i(samplerLocation, 0);
+
+    // ---
+
     glm::mat4 projectionMatrix = glm::perspective(60 * D2R, screenSize.x / screenSize.y, 0.1f, 100.0f);
 
     glm::mat4 modelViewMatrix = glm::scale(glm::mat4(1.0f), glm::vec3(1, 1, 1)); // SCALE
@@ -82,26 +129,12 @@ void Sketch::draw()
 
     glClearColor(0, 0, 1, 1);
     glClear(GL_COLOR_BUFFER_BIT);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vboIds[2]);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, 0);
 }
 
-void Sketch::initShaders()
-{
-    shaderProgram = makeShaderProgram(vss, pss);
-    glUseProgram(shaderProgram);
-
-    positionLocation = glGetAttribLocation(shaderProgram, "a_position");
-    colorLocation = glGetAttribLocation(shaderProgram, "a_color");
-    matrixLocation = glGetUniformLocation(shaderProgram, "u_mvp_matrix");
-
-    glEnableVertexAttribArray(positionLocation);
-    glVertexAttribPointer(positionLocation, 2, GL_FLOAT, GL_FALSE, 0, 0);
-
-    GLfloat color[4] = {1.0f, 1.0f, 0.0f, 1.0f};
-    glVertexAttrib4fv(colorLocation, color);
-}
-
-void Sketch::createBuffers()
+void Sketch::initBuffers()
 {
     const GLfloat vertices[] =
     {
@@ -111,19 +144,87 @@ void Sketch::createBuffers()
         +10, +5, // D
     };
 
+    const GLfloat coords[] =
+    {
+        0, 0, // A
+        0, 1, // B
+        1, 1, // C
+        1, 0, // D
+    };
+
     const GLushort indices[] =
     {
         0, 1, 2,
         2, 3, 0
     };
 
-    glGenBuffers(2, vboIds);
+    glGenBuffers(3, vboIds);
 
     glBindBuffer(GL_ARRAY_BUFFER, vboIds[0]);
     glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 4 * 2, vertices, GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vboIds[1]);
+    glBindBuffer(GL_ARRAY_BUFFER, vboIds[1]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 4 * 2, coords, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vboIds[2]);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLushort) * 6, indices, GL_STATIC_DRAW);
+}
+
+void Sketch::initTextures()
+{
+    fs::path path = "expo67.png";
+
+    stbi_uc *data = nullptr;
+    int x, y, comp;
+
+    if (chr::hasMemoryResources())
+    {
+        auto memoryBuffer = chr::getResourceBuffer(path);
+
+        if (memoryBuffer)
+        {
+            data = stbi_load_from_memory(reinterpret_cast<const stbi_uc*>(memoryBuffer->data()), memoryBuffer->size(), &x, &y, &comp, 0);
+        }
+    }
+    else if (chr::hasFileResources())
+    {
+        auto resPath = chr::getResourcePath(path);
+        data = stbi_load(resPath.string().data(), &x, &y, &comp, 0);
+    }
+
+    if (data)
+    {
+        LOGI << "width: " << x << ", height: " << y << ", comp: " << comp << endl;
+
+        glGenTextures(1, textureIds);
+        glBindTexture(GL_TEXTURE_2D, textureIds[0]);
+
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, x, y, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+
+        #if !defined(CHR_PLATFORM_DESKTOP)
+          glGenerateMipmap(GL_TEXTURE_2D); // FIXME: CRASHES ON DESKTOP
+        #endif
+
+        stbi_image_free(data);
+    }
+    else
+    {
+        LOGE << "ERROR WHILE LOADING IMAGE" << endl;
+    }
+}
+
+void Sketch::initShaders()
+{
+    shaderProgram = makeShaderProgram(vss, pss);
+    glUseProgram(shaderProgram);
+
+    positionLocation = glGetAttribLocation(shaderProgram, "a_position");
+    coordLocation = glGetAttribLocation(shaderProgram, "a_coord");
+    colorLocation = glGetAttribLocation(shaderProgram, "a_color");
+    samplerLocation = glGetUniformLocation(shaderProgram, "u_sampler");
+    matrixLocation = glGetUniformLocation(shaderProgram, "u_mvp_matrix");
 }
 
 // ---
