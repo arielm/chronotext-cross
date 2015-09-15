0d8c60fd06b9d09929217fa45daa96e54b65e5ea
Ariel Malka <ariel@chronotext.org>
2015-09-15 12:19:40 +0300

    COSMETICS...

--- tests/TestingSketch1/src/main.cpp
+++ tests/TestingSketch1/src/main.cpp
@@ -70,9 +70,9 @@ void Sketch::shutdown()
 
 void Sketch::draw()
 {
-    glm::mat4 projectionMatrix = glm::perspective(glm::pi<float>() / 3.0f, screenSize.x / screenSize.y, 0.1f, 100.0f);
+    glm::mat4 projectionMatrix = glm::perspective(60 * D2R, screenSize.x / screenSize.y, 0.1f, 100.0f);
 
-    glm::mat4 modelViewMatrix = glm::scale(glm::mat4(1.0f), glm::vec3(1, 1, 1));
+    glm::mat4 modelViewMatrix = glm::scale(glm::mat4(1.0f), glm::vec3(1, 1, 1)); // SCALE
     modelViewMatrix = glm::translate(modelViewMatrix, glm::vec3(0, 0, -30)); // DISTANCE
     modelViewMatrix = glm::rotate(modelViewMatrix, -15 * D2R, glm::vec3(1, 0, 0)); // ELEVATION
     modelViewMatrix = glm::rotate(modelViewMatrix, (float)getTime(), glm::vec3(0, 1, 0)); // AZIMUTH
@@ -80,6 +80,8 @@ void Sketch::draw()
     glm::mat4 mvp = projectionMatrix * modelViewMatrix;
     glUniformMatrix4fv(matrixLocation, 1, GL_FALSE, &mvp[0][0]);
 
+    // ---
+    
     glClearColor(0, 1, 0, 1);
     glClear(GL_COLOR_BUFFER_BIT);
     glDrawArrays(GL_TRIANGLES, 0, 3);
