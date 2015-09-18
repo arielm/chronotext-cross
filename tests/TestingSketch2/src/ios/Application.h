432d6ace62eeedef1591bcda1dea20e53cd663b6
Ariel Malka <ariel@chronotext.org>
2015-09-19 00:08:21 +0300

    TestingSketch2: PROGRESS...

--- tests/TestingSketch2/src/ios/Application.h
+++ tests/TestingSketch2/src/ios/Application.h
@@ -1,9 +1,14 @@
 
+#pragma once
+
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
+#import "CrossBridge.h"
+
 @interface Application : NSObject <UIApplicationDelegate>
 {
+    CrossBridge *bridge;
     UIWindow *window;
 }
 
--- tests/TestingSketch2/src/ios/Application.mm
+++ tests/TestingSketch2/src/ios/Application.mm
@@ -5,16 +5,29 @@
 
 - (BOOL) application:(UIApplication*)application didFinishLaunchingWithOptions:(NSDictionary*)launchOptions
 {
+    bridge = [[CrossBridge alloc] init];
+
+    bridge.viewControllerProperties =
+    @{
+        GLViewControllerRenderingAPIKey: @(kEAGLRenderingAPIOpenGLES2),
+        GLViewControllerInterfaceOrientationMaskKey: @(UIInterfaceOrientationMaskLandscape),
+        GLViewControllerDepthFormatKey: @(GLKViewDrawableDepthFormatNone),
+    };
+
     window = [[UIWindow alloc] initWithFrame:UIScreen.mainScreen.bounds];
-    window.backgroundColor = [UIColor redColor];
+    window.backgroundColor = [UIColor blackColor];
+
+    [window setRootViewController:bridge.viewController];
     [window makeKeyAndVisible];
-    
+
     return YES;
 }
 
 - (void) dealloc
 {
     [window release];
+    [bridge release];
+
     [super dealloc];
 }
 
