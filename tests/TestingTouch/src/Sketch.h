44ac9a08cc5407c4bd4f16a78f844a2a87580eac
Ariel Malka <ariel@chronotext.org>
2015-09-23 14:10:48 +0300

    STARTING WITH TestingTouch PROJECT...

---
+++ tests/TestingTouch/src/Sketch.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "cross/CrossSketch.h"
+
+class Sketch : public chr::CrossSketch
+{
+public:
+  virtual ~Sketch() {}
+
+  void setup() final;
+  void shutdown() final;
+  void draw() final;
+
+  void initBuffers();
+  void initTextures();
+  void initShaders();
+
+protected:
+  GLuint shaderProgram;
+  GLint positionLocation;
+  GLint coordLocation;
+  GLint colorLocation;
+  GLint samplerLocation;
+  GLint matrixLocation;
+
+  GLuint vboIds[3];
+  GLuint textureIds[1];
+
+  GLint maxAnisotropy;
+};
