6a90fa2291dd801d6665c747ce7b34e74eaa9a1b
Ariel Malka <ariel@chronotext.org>
2015-10-03 22:12:46 +0300

    EMSCRIPTEN: REDUCING EXECUTABLE SIZE

--- core/cmake/test.cmake
+++ core/cmake/test.cmake
@@ -154,7 +154,7 @@ elseif (PLATFORM MATCHES emscripten)
 
     configure_file("${CROSS_ROOT}/cmake/emscripten/template.html.in" template.html)
     set(CMAKE_EXECUTABLE_SUFFIX .html)
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --emrun --shell-file template.html")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Os --closure 1 --emrun --shell-file template.html")
 
     if (RESOURCE_COUNT)
       set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --preload-file ../../res")
