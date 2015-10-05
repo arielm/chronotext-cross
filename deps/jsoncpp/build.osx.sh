8031f9d29745ed2f7b2a0feb476c1ab06b85b492
Ariel Malka <ariel@chronotext.org>
2015-10-05 17:32:30 +0300

    INCLUDING deps/jsoncpp

---
+++ deps/jsoncpp/build.osx.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+PLATFORM="osx"
+
+SRC_DIR="build/src"
+BUILD_DIR="build/$PLATFORM"
+INSTALL_DIR="dist/$PLATFORM"
+
+SRC_PATH="$(pwd)/$SRC_DIR"
+INSTALL_PATH="$(pwd)/$INSTALL_DIR"
+
+if [ ! -d "$SRC_PATH" ]; then
+  echo "SOURCE NOT FOUND!"
+  exit 1
+fi
+
+# ---
+
+TOOLCHAIN_FILE="$CROSS_PATH/core/cmake/toolchains/osx.cmake"
+
+cmake -H"$SRC_DIR" -B"$BUILD_DIR" \
+  -DCMAKE_TOOLCHAIN_FILE="$TOOLCHAIN_FILE" -G Ninja \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DLIBRARY_OUTPUT_PATH="$INSTALL_PATH/lib" \
+  -DJSONCPP_WITH_TESTS=OFF -DJSONCPP_WITH_POST_BUILD_UNITTEST=OFF -DJSONCPP_WITH_PKGCONFIG_SUPPORT=OFF
+
+if [ $? != 0 ]; then
+  echo "CONFIGURATION FAILED!"
+  exit -1
+fi
+
+# ---
+
+rm -rf "$INSTALL_PATH" # XXX: REQUIRED?
+cmake --build "$BUILD_DIR"
+
+if [ $? != 0 ]; then
+  echo "BUILD FAILED!"
+  exit -1
+fi
+
+cd "$INSTALL_PATH"
+ln -s "$SRC_PATH/include"
