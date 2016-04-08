ad172d1bba7960cf3b44d44e6dd0691816d480b1
Ariel Malka <ariel@chronotext.org>
2016-04-08 12:02:54 +0300

    gl/Buffer: USING A buffer NAMESPACE FOR INTERNALS

--- core/src/gl/Buffer.cpp
+++ core/src/gl/Buffer.cpp
@@ -6,32 +6,35 @@ namespace chr
 {
   namespace gl
   {
-    int TypeTraits<Vertex<>>::usageCounter = 0;
-    int TypeTraits<Vertex<UV>>::usageCounter = 0;
-    int TypeTraits<Vertex<N>>::usageCounter = 0;
-    int TypeTraits<Vertex<RGBA>>::usageCounter = 0;
-    int TypeTraits<Vertex<N|UV>>::usageCounter = 0;
-    int TypeTraits<Vertex<N|UV|RGBA>>::usageCounter = 0;
-    int TypeTraits<Vertex<UV|RGBA>>::usageCounter = 0;
-    int TypeTraits<Vertex<N|RGBA>>::usageCounter = 0;
-    int TypeTraits<glm::vec2>::usageCounter = 0;
-    int TypeTraits<glm::vec3>::usageCounter = 0;
-    int TypeTraits<glm::vec4>::usageCounter = 0;
-    int TypeTraits<GLushort>::usageCounter = 0;
-    int TypeTraits<GLuint>::usageCounter = 0;
+    namespace buffer
+    {
+      int TypeTraits<Vertex<>>::usageCounter = 0;
+      int TypeTraits<Vertex<UV>>::usageCounter = 0;
+      int TypeTraits<Vertex<N>>::usageCounter = 0;
+      int TypeTraits<Vertex<RGBA>>::usageCounter = 0;
+      int TypeTraits<Vertex<N | UV>>::usageCounter = 0;
+      int TypeTraits<Vertex<N | UV | RGBA>>::usageCounter = 0;
+      int TypeTraits<Vertex<UV | RGBA>>::usageCounter = 0;
+      int TypeTraits<Vertex<N | RGBA>>::usageCounter = 0;
+      int TypeTraits<glm::vec2>::usageCounter = 0;
+      int TypeTraits<glm::vec3>::usageCounter = 0;
+      int TypeTraits<glm::vec4>::usageCounter = 0;
+      int TypeTraits<GLushort>::usageCounter = 0;
+      int TypeTraits<GLuint>::usageCounter = 0;
 
-    unordered_map<int, Element<Vertex<>>> TypeTraits<Vertex<>>::map;
-    unordered_map<int, Element<Vertex<UV>>> TypeTraits<Vertex<UV>>::map;
-    unordered_map<int, Element<Vertex<N>>> TypeTraits<Vertex<N>>::map;
-    unordered_map<int, Element<Vertex<RGBA>>> TypeTraits<Vertex<RGBA>>::map;
-    unordered_map<int, Element<Vertex<N|UV>>> TypeTraits<Vertex<N|UV>>::map;
-    unordered_map<int, Element<Vertex<N|UV|RGBA>>> TypeTraits<Vertex<N|UV|RGBA>>::map;
-    unordered_map<int, Element<Vertex<UV|RGBA>>> TypeTraits<Vertex<UV|RGBA>>::map;
-    unordered_map<int, Element<Vertex<N|RGBA>>> TypeTraits<Vertex<N|RGBA>>::map;
-    unordered_map<int, Element<glm::vec2>> TypeTraits<glm::vec2>::map;
-    unordered_map<int, Element<glm::vec3>> TypeTraits<glm::vec3>::map;
-    unordered_map<int, Element<glm::vec4>> TypeTraits<glm::vec4>::map;
-    unordered_map<int, Element<GLushort>> TypeTraits<GLushort>::map;
-    unordered_map<int, Element<GLuint>> TypeTraits<GLuint>::map;
+      unordered_map<int, Element<Vertex<>>> TypeTraits<Vertex<>>::map;
+      unordered_map<int, Element<Vertex<UV>>> TypeTraits<Vertex<UV>>::map;
+      unordered_map<int, Element<Vertex<N>>> TypeTraits<Vertex<N>>::map;
+      unordered_map<int, Element<Vertex<RGBA>>> TypeTraits<Vertex<RGBA>>::map;
+      unordered_map<int, Element<Vertex<N | UV>>> TypeTraits<Vertex<N | UV>>::map;
+      unordered_map<int, Element<Vertex<N | UV | RGBA>>> TypeTraits<Vertex<N | UV | RGBA>>::map;
+      unordered_map<int, Element<Vertex<UV | RGBA>>> TypeTraits<Vertex<UV | RGBA>>::map;
+      unordered_map<int, Element<Vertex<N | RGBA>>> TypeTraits<Vertex<N | RGBA>>::map;
+      unordered_map<int, Element<glm::vec2>> TypeTraits<glm::vec2>::map;
+      unordered_map<int, Element<glm::vec3>> TypeTraits<glm::vec3>::map;
+      unordered_map<int, Element<glm::vec4>> TypeTraits<glm::vec4>::map;
+      unordered_map<int, Element<GLushort>> TypeTraits<GLushort>::map;
+      unordered_map<int, Element<GLuint>> TypeTraits<GLuint>::map;
+    }
   }
 }
--- core/src/gl/Buffer.h
+++ core/src/gl/Buffer.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "gl/gl.h"
 #include "gl/Vertex.h"
 #include "gl/ShaderProgram.h"
 
@@ -10,54 +9,57 @@ namespace chr
 {
   namespace gl
   {
-    enum TypeIndex : size_t
+    namespace buffer
     {
-      VERTEX,
-      VERTEX_UV,
-      VERTEX_N,
-      VERTEX_RGBA,
-      VERTEX_N_UV,
-      VERTEX_N_UV_RGBA,
-      VERTEX_UV_RGBA,
-      VERTEX_N_RGBA,
-      VEC2,
-      VEC3,
-      VEC4,
-      GLUSHORT,
-      GLUINT,
-    };
+      enum TypeIndex : size_t
+      {
+        VERTEX,
+        VERTEX_UV,
+        VERTEX_N,
+        VERTEX_RGBA,
+        VERTEX_N_UV,
+        VERTEX_N_UV_RGBA,
+        VERTEX_UV_RGBA,
+        VERTEX_N_RGBA,
+        VEC2,
+        VEC3,
+        VEC4,
+        GLUSHORT,
+        GLUINT,
+      };
 
-    template<class T>
-    struct TypeTraits
-    {};
+      template<class T>
+      struct TypeTraits
+      {};
 
-    template<class T>
-    struct Element
-    {
-      std::vector<T> storage;
-      size_t allocatedSize;
-      int useCount;
-      GLuint vboId;
-    };
+      template<class T>
+      struct Element
+      {
+        std::vector<T> storage;
+        size_t allocatedSize;
+        int useCount;
+        GLuint vboId;
+      };
+    }
 
     template<typename T>
     class Buffer
     {
     public:
-      static constexpr size_t typeIndex = TypeTraits<T>::typeIndex;
-      static constexpr GLenum target = TypeTraits<T>::target;
+      static constexpr size_t typeIndex = buffer::TypeTraits<T>::typeIndex;
+      static constexpr GLenum target = buffer::TypeTraits<T>::target;
 
       GLenum usage;
       int bufferId;
-      Element<T> &element;
+      buffer::Element<T> &element;
       std::vector<T> &storage;
       std::vector<GLuint> locations;
 
       Buffer(GLenum usage = GL_DYNAMIC_DRAW)
       :
       usage(usage),
-      bufferId(TypeTraits<T>::usageCounter),
-      element(TypeTraits<T>::map.emplace(TypeTraits<T>::usageCounter++, Element<T>()).first->second),
+      bufferId(buffer::TypeTraits<T>::usageCounter),
+      element(buffer::TypeTraits<T>::map.emplace(buffer::TypeTraits<T>::usageCounter++, buffer::Element<T>()).first->second),
       storage(element.storage)
       {
         element.useCount++;
@@ -90,7 +92,7 @@ namespace chr
 
       ~Buffer()
       {
-        auto it = TypeTraits<T>::map.find(bufferId);
+        auto it = buffer::TypeTraits<T>::map.find(bufferId);
         it->second.useCount--;
 
         if (it->second.useCount == 0)
@@ -100,7 +102,7 @@ namespace chr
             glDeleteBuffers(1, &it->second.vboId);
           }
 
-          TypeTraits<T>::map.erase(it);
+          buffer::TypeTraits<T>::map.erase(it);
         }
       }
 
@@ -138,12 +140,12 @@ namespace chr
 
         glBindBuffer(target, element.vboId);
 
-        locations = TypeTraits<T>::bindAttributes(shader);
+        locations = buffer::TypeTraits<T>::bindAttributes(shader);
       }
 
       void unbind()
       {
-        TypeTraits<T>::unbindAttributes(locations);
+        buffer::TypeTraits<T>::unbindAttributes(locations);
         glBindBuffer(target, 0);
       }
 
@@ -187,191 +189,194 @@ namespace chr
 
     // ---
 
-    template <>
-    struct TypeTraits<Vertex<>>
+    namespace buffer
     {
-      static constexpr size_t typeIndex = TypeIndex::VERTEX;
-      static constexpr GLenum target = GL_ARRAY_BUFFER;
-
-      static int usageCounter;
-      static std::unordered_map<int, Element<Vertex<>>> map;
-
-      static std::vector<GLuint> bindAttributes(const ShaderProgram &shader)
+      template<>
+      struct TypeTraits<Vertex<>>
       {
-        glEnableVertexAttribArray(shader.positionLocation);
-        glVertexAttribPointer(shader.positionLocation, 3, GL_FLOAT, GL_FALSE, 0, 0);
+        static constexpr size_t typeIndex = TypeIndex::VERTEX;
+        static constexpr GLenum target = GL_ARRAY_BUFFER;
 
-        return { shader.positionLocation };
-      }
+        static int usageCounter;
+        static std::unordered_map<int, Element<Vertex<>>> map;
 
-      static void unbindAttributes(const std::vector<GLuint> &locations)
+        static std::vector<GLuint> bindAttributes(const ShaderProgram &shader)
+        {
+          glEnableVertexAttribArray(shader.positionLocation);
+          glVertexAttribPointer(shader.positionLocation, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+          return {shader.positionLocation};
+        }
+
+        static void unbindAttributes(const std::vector<GLuint> &locations)
+        {
+          glDisableVertexAttribArray(locations[0]);
+        }
+      };
+
+      template<>
+      struct TypeTraits<Vertex<UV>>
       {
-        glDisableVertexAttribArray(locations[0]);
-      }
-    };
+        static constexpr size_t typeIndex = TypeIndex::VERTEX_UV;
+        static constexpr GLenum target = GL_ARRAY_BUFFER;
 
-    template <>
-    struct TypeTraits<Vertex<UV>>
-    {
-      static constexpr size_t typeIndex = TypeIndex::VERTEX_UV;
-      static constexpr GLenum target = GL_ARRAY_BUFFER;
+        static int usageCounter;
+        static std::unordered_map<int, Element<Vertex<UV>>> map;
 
-      static int usageCounter;
-      static std::unordered_map<int, Element<Vertex<UV>>> map;
+        static std::vector<GLuint> bindAttributes(const ShaderProgram &shader)
+        {
+          glEnableVertexAttribArray(shader.positionLocation);
+          glVertexAttribPointer(shader.positionLocation, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex<UV>), 0);
 
-      static std::vector<GLuint> bindAttributes(const ShaderProgram &shader)
+          glEnableVertexAttribArray(shader.coordLocation);
+          glVertexAttribPointer(shader.coordLocation, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex<UV>), (GLvoid *) sizeof(Vertex<>));
+
+          return {shader.positionLocation, shader.coordLocation};
+        }
+
+        static void unbindAttributes(const std::vector<GLuint> &locations)
+        {
+          glDisableVertexAttribArray(locations[0]);
+          glDisableVertexAttribArray(locations[1]);
+        }
+      };
+
+      template<>
+      struct TypeTraits<Vertex<N>>
       {
-        glEnableVertexAttribArray(shader.positionLocation);
-        glVertexAttribPointer(shader.positionLocation, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex<UV>), 0);
+        static constexpr size_t typeIndex = TypeIndex::VERTEX_N;
+        static constexpr GLenum target = GL_ARRAY_BUFFER;
 
-        glEnableVertexAttribArray(shader.coordLocation);
-        glVertexAttribPointer(shader.coordLocation, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex<UV>), (GLvoid *) sizeof(Vertex<>));
+        static int usageCounter;
+        static std::unordered_map<int, Element<Vertex<N>>> map;
+      };
 
-        return { shader.positionLocation, shader.coordLocation };
-      }
-
-      static void unbindAttributes(const std::vector<GLuint> &locations)
+      template<>
+      struct TypeTraits<Vertex<RGBA>>
       {
-        glDisableVertexAttribArray(locations[0]);
-        glDisableVertexAttribArray(locations[1]);
-      }
-    };
+        static constexpr size_t typeIndex = TypeIndex::VERTEX_RGBA;
+        static constexpr GLenum target = GL_ARRAY_BUFFER;
 
-    template <>
-    struct TypeTraits<Vertex<N>>
-    {
-      static constexpr size_t typeIndex = TypeIndex::VERTEX_N;
-      static constexpr GLenum target = GL_ARRAY_BUFFER;
+        static int usageCounter;
+        static std::unordered_map<int, Element<Vertex<RGBA>>> map;
+      };
 
-      static int usageCounter;
-      static std::unordered_map<int, Element<Vertex<N>>> map;
-    };
-
-    template <>
-    struct TypeTraits<Vertex<RGBA>>
-    {
-      static constexpr size_t typeIndex = TypeIndex::VERTEX_RGBA;
-      static constexpr GLenum target = GL_ARRAY_BUFFER;
-
-      static int usageCounter;
-      static std::unordered_map<int, Element<Vertex<RGBA>>> map;
-    };
-
-    template <>
-    struct TypeTraits<Vertex<N|UV>>
-    {
-      static constexpr size_t typeIndex = TypeIndex::VERTEX_N_UV;
-      static constexpr GLenum target = GL_ARRAY_BUFFER;
-
-      static int usageCounter;
-      static std::unordered_map<int, Element<Vertex<N|UV>>> map;
-    };
-
-    template <>
-    struct TypeTraits<Vertex<N|UV|RGBA>>
-    {
-    public:
-      static constexpr size_t typeIndex = TypeIndex::VERTEX_N_UV_RGBA;
-      static constexpr GLenum target = GL_ARRAY_BUFFER;
-
-      static int usageCounter;
-      static std::unordered_map<int, Element<Vertex<N|UV|RGBA>>> map;
-    };
-
-    template <>
-    struct TypeTraits<Vertex<UV|RGBA>>
-    {
-      static constexpr size_t typeIndex = TypeIndex::VERTEX_UV_RGBA;
-      static constexpr GLenum target = GL_ARRAY_BUFFER;
-
-      static int usageCounter;
-      static std::unordered_map<int, Element<Vertex<UV|RGBA>>> map;
-    };
-
-    template <>
-    struct TypeTraits<Vertex<N|RGBA>>
-    {
-      static constexpr size_t typeIndex = TypeIndex::VERTEX_N_RGBA;
-      static constexpr GLenum target = GL_ARRAY_BUFFER;
-
-      static int usageCounter;
-      static std::unordered_map<int, Element<Vertex<N|RGBA>>> map;
-    };
-
-    template <>
-    struct TypeTraits<glm::vec2>
-    {
-      static constexpr size_t typeIndex = TypeIndex::VEC2;
-      static constexpr GLenum target = GL_ARRAY_BUFFER;
-
-      static int usageCounter;
-      static std::unordered_map<int, Element<glm::vec2>> map;
-    };
-
-    template <>
-    struct TypeTraits<glm::vec3>
-    {
-      static constexpr size_t typeIndex = TypeIndex::VEC3;
-      static constexpr GLenum target = GL_ARRAY_BUFFER;
-
-      static int usageCounter;
-      static std::unordered_map<int, Element<glm::vec3>> map;
-
-      static std::vector<GLuint> bindAttributes(const ShaderProgram &shader)
+      template<>
+      struct TypeTraits<Vertex<N | UV>>
       {
-        glEnableVertexAttribArray(shader.positionLocation);
-        glVertexAttribPointer(shader.positionLocation, 3, GL_FLOAT, GL_FALSE, 0, 0);
+        static constexpr size_t typeIndex = TypeIndex::VERTEX_N_UV;
+        static constexpr GLenum target = GL_ARRAY_BUFFER;
 
-        return { shader.positionLocation };
-      }
+        static int usageCounter;
+        static std::unordered_map<int, Element<Vertex<N | UV>>> map;
+      };
 
-      static void unbindAttributes(const std::vector<GLuint> &locations)
+      template<>
+      struct TypeTraits<Vertex<N | UV | RGBA>>
       {
-        glDisableVertexAttribArray(locations[0]);
-      }
-    };
+      public:
+        static constexpr size_t typeIndex = TypeIndex::VERTEX_N_UV_RGBA;
+        static constexpr GLenum target = GL_ARRAY_BUFFER;
 
-    template <>
-    struct TypeTraits<glm::vec4>
-    {
-      static constexpr size_t typeIndex = TypeIndex::VEC4;
-      static constexpr GLenum target = GL_ARRAY_BUFFER;
+        static int usageCounter;
+        static std::unordered_map<int, Element<Vertex<N | UV | RGBA>>> map;
+      };
 
-      static int usageCounter;
-      static std::unordered_map<int, Element<glm::vec4>> map;
-
-      static std::vector<GLuint> bindAttributes(const ShaderProgram &shader)
+      template<>
+      struct TypeTraits<Vertex<UV | RGBA>>
       {
-        glEnableVertexAttribArray(shader.positionLocation);
-        glVertexAttribPointer(shader.positionLocation, 4, GL_FLOAT, GL_FALSE, 0, 0);
+        static constexpr size_t typeIndex = TypeIndex::VERTEX_UV_RGBA;
+        static constexpr GLenum target = GL_ARRAY_BUFFER;
 
-        return { shader.positionLocation };
-      }
+        static int usageCounter;
+        static std::unordered_map<int, Element<Vertex<UV | RGBA>>> map;
+      };
 
-      static void unbindAttributes(const std::vector<GLuint> &locations)
+      template<>
+      struct TypeTraits<Vertex<N | RGBA>>
       {
-        glDisableVertexAttribArray(locations[0]);
-      }
-    };
+        static constexpr size_t typeIndex = TypeIndex::VERTEX_N_RGBA;
+        static constexpr GLenum target = GL_ARRAY_BUFFER;
 
-    template <>
-    struct TypeTraits<GLushort>
-    {
-      static constexpr size_t typeIndex = TypeIndex::GLUSHORT;
-      static constexpr GLenum target = GL_ELEMENT_ARRAY_BUFFER;
+        static int usageCounter;
+        static std::unordered_map<int, Element<Vertex<N | RGBA>>> map;
+      };
 
-      static int usageCounter;
-      static std::unordered_map<int, Element<GLushort>> map;
-    };
+      template<>
+      struct TypeTraits<glm::vec2>
+      {
+        static constexpr size_t typeIndex = TypeIndex::VEC2;
+        static constexpr GLenum target = GL_ARRAY_BUFFER;
 
-    template <>
-    struct TypeTraits<GLuint>
-    {
-      static constexpr size_t typeIndex = TypeIndex::GLUINT;
-      static constexpr GLenum target = GL_ELEMENT_ARRAY_BUFFER;
+        static int usageCounter;
+        static std::unordered_map<int, Element<glm::vec2>> map;
+      };
 
-      static int usageCounter;
-      static std::unordered_map<int, Element<GLuint>> map;
-    };
+      template<>
+      struct TypeTraits<glm::vec3>
+      {
+        static constexpr size_t typeIndex = TypeIndex::VEC3;
+        static constexpr GLenum target = GL_ARRAY_BUFFER;
+
+        static int usageCounter;
+        static std::unordered_map<int, Element<glm::vec3>> map;
+
+        static std::vector<GLuint> bindAttributes(const ShaderProgram &shader)
+        {
+          glEnableVertexAttribArray(shader.positionLocation);
+          glVertexAttribPointer(shader.positionLocation, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+          return {shader.positionLocation};
+        }
+
+        static void unbindAttributes(const std::vector<GLuint> &locations)
+        {
+          glDisableVertexAttribArray(locations[0]);
+        }
+      };
+
+      template<>
+      struct TypeTraits<glm::vec4>
+      {
+        static constexpr size_t typeIndex = TypeIndex::VEC4;
+        static constexpr GLenum target = GL_ARRAY_BUFFER;
+
+        static int usageCounter;
+        static std::unordered_map<int, Element<glm::vec4>> map;
+
+        static std::vector<GLuint> bindAttributes(const ShaderProgram &shader)
+        {
+          glEnableVertexAttribArray(shader.positionLocation);
+          glVertexAttribPointer(shader.positionLocation, 4, GL_FLOAT, GL_FALSE, 0, 0);
+
+          return {shader.positionLocation};
+        }
+
+        static void unbindAttributes(const std::vector<GLuint> &locations)
+        {
+          glDisableVertexAttribArray(locations[0]);
+        }
+      };
+
+      template<>
+      struct TypeTraits<GLushort>
+      {
+        static constexpr size_t typeIndex = TypeIndex::GLUSHORT;
+        static constexpr GLenum target = GL_ELEMENT_ARRAY_BUFFER;
+
+        static int usageCounter;
+        static std::unordered_map<int, Element<GLushort>> map;
+      };
+
+      template<>
+      struct TypeTraits<GLuint>
+      {
+        static constexpr size_t typeIndex = TypeIndex::GLUINT;
+        static constexpr GLenum target = GL_ELEMENT_ARRAY_BUFFER;
+
+        static int usageCounter;
+        static std::unordered_map<int, Element<GLuint>> map;
+      };
+    }
   }
 }
