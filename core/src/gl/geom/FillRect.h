e6c26677d0b21132f201a3e9a62a9e50ec4d8839
Ariel Malka <ariel@chronotext.org>
2016-04-15 20:03:22 +0300

    PROGRESS WITH gl/geom/FillRect

--- core/src/gl/geom/FillRect.h
+++ core/src/gl/geom/FillRect.h
@@ -29,9 +29,10 @@ namespace chr
         template<int Orientation=GL_CCW>
         inline void draw(Matrix &matrix, float x, float y, float width, float height) const
         {
-          matrix.addTransformedQuad<GL_TRIANGLES, Orientation>(Quad<V>(math::Rectf(x, y, width, height)), batch);
+          draw<Orientation>(matrix, math::Rectf(x, y, width, height));
         }
-        
+
+        template<int Orientation=GL_CCW>
         void draw(const math::Rectf &rect) const
         {
           batch.addVertex(rect.x1, rect.y1);
@@ -39,13 +40,22 @@ namespace chr
           batch.addVertex(rect.x2, rect.y2);
           batch.addVertex(rect.x2, rect.y1);
 
-          batch.addIndices(0, 3, 2, 2, 1, 0);
+          if (Orientation == GL_CCW)
+          {
+            batch.addIndices(0, 1, 2, 2, 3, 0);
+          }
+          else
+          {
+            batch.addIndices(0, 3, 2, 2, 1, 0);
+          }
+          
           batch.incrementIndices(4);
         }
 
+        template<int Orientation=GL_CCW>
         inline void draw(float x, float y, float width, float height) const
         {
-          draw(math::Rectf(x, y, width, height));
+          draw<Orientation>(math::Rectf(x, y, width, height));
         }
       };
     }
