560996df70d209e39aa7a75807561d7c60f9ad27
Ariel Malka <ariel@chronotext.org>
2016-04-16 00:22:01 +0300

    1) ADJUSTING FillRect TO TAKE x1, y1, x2, y2 INPUT 2) STARTING WITH FillCircle

---
+++ core/src/gl/geom/FillCircle.h
@@ -0,0 +1,93 @@
+#pragma once
+
+#include "gl/Batch.h"
+#include "gl/Matrix.h"
+
+namespace chr
+{
+  namespace gl
+  {
+    namespace geom
+    {
+      template<int V = 0, typename I = GLushort>
+      class FillCircle
+      {
+      public:
+        IndexedVertexBatch<V,I> &batch;
+
+        FillCircle(IndexedVertexBatch<V,I> &batch)
+        :
+        batch(batch)
+        {}
+
+        template<int Orientation=GL_CCW>
+        void draw(Matrix &matrix, float x, float y, float r, float a0 = 0, float a1 = TWO_PI, float dd = 8) const
+        {
+          float aa = fabsf(a1 - a0);
+          int n = ceilf(aa * r / dd) + 1;
+
+          // TODO: batch.reserve(n + 1);
+
+          batch.addVertex(matrix.transformPoint(x, y));
+
+          for (int i = 0; i < n; i++)
+          {
+            float d = fmin(aa, i * dd / r);
+            float xx = x + sinf(a0 + d) * r;
+            float yy = y + cosf(a0 + d) * r;
+
+            batch.addVertex(matrix.transformPoint(xx, yy));
+
+            if (i < n - 1)
+            {
+              if (Orientation == GL_CCW)
+              {
+                batch.addIndices(i + 1, 0, i + 2);
+              }
+              else
+              {
+                batch.addIndices(0, i + 1, i + 2);
+              }
+            }
+          }
+
+          batch.incrementIndices(n + 1);
+        }
+
+        template<int Orientation=GL_CCW>
+        void draw(float x, float y, float r, float a0 = 0, float a1 = TWO_PI, float dd = 8) const
+        {
+          float aa = fabsf(a1 - a0);
+          int n = ceilf(aa * r / dd) + 1;
+
+          // TODO: batch.reserve(n + 1);
+
+          batch.addVertex(x, y);
+
+          for (int i = 0; i < n; i++)
+          {
+            float d = fmin(aa, i * dd / r);
+            float xx = x + sinf(a0 + d) * r;
+            float yy = y + cosf(a0 + d) * r;
+
+            batch.addVertex(xx, yy);
+
+            if (i < n)
+            {
+              if (Orientation == GL_CCW)
+              {
+                batch.addIndices(i + 1, 0, i + 2);
+              }
+              else
+              {
+                batch.addIndices(0, i + 1, i + 2);
+              }
+            }
+          }
+          
+          batch.incrementIndices(n + 1);
+        }
+      };
+    }
+  }
+}
--- core/src/gl/geom/FillRect.h
+++ core/src/gl/geom/FillRect.h
@@ -27,9 +27,9 @@ namespace chr
         }
 
         template<int Orientation=GL_CCW>
-        inline void draw(Matrix &matrix, float x, float y, float width, float height) const
+        inline void draw(Matrix &matrix, float x1, float y1, float x2, float y2) const
         {
-          draw<Orientation>(matrix, math::Rectf(x, y, width, height));
+          matrix.addTransformedQuad<GL_TRIANGLES, Orientation>(Quad<V>(x1, y1, x2, y2), batch);
         }
 
         template<int Orientation=GL_CCW>
@@ -53,9 +53,9 @@ namespace chr
         }
 
         template<int Orientation=GL_CCW>
-        inline void draw(float x, float y, float width, float height) const
+        inline void draw(float x1, float y1, float x2, float y2) const
         {
-          draw<Orientation>(math::Rectf(x, y, width, height));
+          draw<Orientation>(math::Rectf(glm::vec2(x1, y1), glm::vec2(x2, y2)));
         }
       };
     }
