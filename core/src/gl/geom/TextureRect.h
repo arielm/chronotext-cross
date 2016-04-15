b23041e6bce591c20eb4c48283a51f5ceca445c2
Ariel Malka <ariel@chronotext.org>
2016-04-16 01:10:36 +0300

    STARTING WITH gl/geom/TextureRect

---
+++ core/src/gl/geom/TextureRect.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include "gl/Batch.h"
+#include "gl/Matrix.h"
+
+namespace chr
+{
+  namespace gl
+  {
+    namespace geom
+    {
+      template<int V = 0, typename I = GLushort>
+      class TextureRect
+      {
+      public:
+        IndexedVertexBatch<V,I> &batch;
+
+        TextureRect(IndexedVertexBatch<V,I> &batch)
+        :
+        batch(batch)
+        {}
+
+        template<int Orientation=GL_CCW>
+        void draw(Matrix &matrix, float x = 0, float y = 0)
+        {
+          float x2 = x + batch.texture.width * batch.texture.maxU;
+          float y2 = y + batch.texture.height * batch.texture.maxV;
+
+          float u2 = batch.texture.maxU;
+          float v2 = batch.texture.maxV;
+
+          matrix.addTransformedQuad<GL_TRIANGLES, Orientation>(Quad<UV>(x, y, x2, y2, 0, 0, u2, v2), batch);
+
+          batch.addIndices(0, 1, 2, 2, 3, 0);
+          batch.incrementIndices(4);
+        }
+
+        template<int Orientation=GL_CCW>
+        void draw(float x = 0, float y = 0)
+        {
+          float x2 = x + batch.texture.width * batch.texture.maxU;
+          float y2 = y + batch.texture.height * batch.texture.maxV;
+
+          float u2 = batch.texture.maxU;
+          float v2 = batch.texture.maxV;
+
+          batch.addVertex( x,  y, 0,  0,  0);
+          batch.addVertex( x, y2, 0,  0, v2);
+          batch.addVertex(x2, y2, 0, u2, v2);
+          batch.addVertex(x2,  y, 0, u2,  0);
+
+          if (Orientation == GL_CCW)
+          {
+            batch.addIndices(0, 1, 2, 2, 3, 0);
+          }
+          else
+          {
+            batch.addIndices(0, 3, 2, 2, 1, 0);
+          }
+
+          batch.incrementIndices(4);
+        }
+      };
+    }
+  }
+}
