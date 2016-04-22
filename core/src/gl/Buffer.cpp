d315a9bc400f703199a69601ec0b9e40e2dec33e
Ariel Malka <ariel@chronotext.org>
2016-04-22 15:47:33 +0300

    RELATED TO PREVIOUS COMMIT...

--- core/src/gl/Buffer.cpp
+++ core/src/gl/Buffer.cpp
@@ -11,11 +11,11 @@ namespace chr
       int TypeTraits<Vertex<XYZ>>::usageCounter = 0;
       int TypeTraits<Vertex<XYZ.UV>>::usageCounter = 0;
       int TypeTraits<Vertex<XYZ.RGBA>>::usageCounter = 0;
-//      int TypeTraits<Vertex<XYZ.N>>::usageCounter = 0;
+      int TypeTraits<Vertex<XYZ.N>>::usageCounter = 0;
       int TypeTraits<Vertex<XYZ.UV.RGBA>>::usageCounter = 0;
-//      int TypeTraits<Vertex<XYZ.UV.N>>::usageCounter = 0;
-//      int TypeTraits<Vertex<XYZ.RGBA.N>>::usageCounter = 0;
-//      int TypeTraits<Vertex<XYZ.UV.RGBA.N>>::usageCounter = 0;
+      int TypeTraits<Vertex<XYZ.UV.N>>::usageCounter = 0;
+      int TypeTraits<Vertex<XYZ.RGBA.N>>::usageCounter = 0;
+      int TypeTraits<Vertex<XYZ.UV.RGBA.N>>::usageCounter = 0;
       int TypeTraits<glm::vec2>::usageCounter = 0;
       int TypeTraits<glm::vec3>::usageCounter = 0;
       int TypeTraits<glm::vec4>::usageCounter = 0;
