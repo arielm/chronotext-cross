c95452487d4f7b8cbac136d80113dca8fd3ca2c2
Ariel Malka <ariel@chronotext.org>
2016-05-15 22:03:52 +0300

    ATTEMPT TO INCORPORATE CGAL. PROBLEM: NOT iOS NOR ANDROID FRIENDLY...
    (OSX AND EMSCRIPTEN SEEM TO WORK)

---
+++ deps/cgal/build.osx.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+PLATFORM="osx"
+
+SRC_DIR="build/src"
+BUILD_DIR="build/$PLATFORM"
+INSTALL_DIR="dist/$PLATFORM"
+
+SRC_PATH="$(pwd)/$SRC_DIR"
+INSTALL_PATH="$(pwd)/$INSTALL_DIR"
+
+if [ ! -d "$SRC_PATH" ]; then
+  echo "SOURCE NOT FOUND!"
+  exit 1
+fi
+
+# ---
+
+TOOLCHAIN_FILE="$CROSS_PATH/core/cmake/toolchains/osx.cmake"
+
+cmake -H"$SRC_DIR" -B"$BUILD_DIR" \
+  -DCMAKE_TOOLCHAIN_FILE="$TOOLCHAIN_FILE" -G Ninja \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DBUILD_SHARED_LIBS=OFF \
+  -DWITH_CGAL_Core=OFF \
+  -DWITH_CGAL_Qt5=OFF \
+  -DWITH_CGAL_ImageIO=OFF \
+  -DCGAL_DISABLE_GMP=ON \
+  -DBoost_INCLUDE_DIR="$CROSS_PATH/deps/boost/dist/$PLATFORM/include" \
+  -DBoost_LIBRARY_DIR="$CROSS_PATH/deps/boost/dist/$PLATFORM/lib" \
+  -DCMAKE_CXX_FLAGS="-DCGAL_HAS_NO_THREADS"
+
+if [ $? != 0 ]; then
+  echo "CONFIGURATION FAILED!"
+  exit -1
+fi
+
+# ---
+
+cmake --build "$BUILD_DIR"
+
+if [ $? != 0 ]; then
+  echo "BUILD FAILED!"
+  exit -1
+fi
+
+# ---
+
+rm -rf "$INSTALL_PATH"
+mkdir -p "$INSTALL_PATH/lib"
+mv "$BUILD_DIR/lib/libCGAL.a" "$INSTALL_PATH/lib"
+cp -r "$BUILD_DIR/include" "$INSTALL_PATH"
+
+cd "$INSTALL_PATH"
+ln -s "$SRC_PATH" "src"
