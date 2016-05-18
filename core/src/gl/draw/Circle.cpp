49d54397155bd493c9045905cf6c62acf5170d1d
Ariel Malka <ariel@chronotext.org>
2016-05-18 11:36:27 +0300

    gl/draw/Circle: ADDING SUPPORT FOR TEXTURES

--- core/src/gl/draw/Circle.cpp
+++ core/src/gl/draw/Circle.cpp
@@ -12,6 +12,24 @@ namespace chr
         return *this;
       }
 
+      Circle& Circle::setTextureOffset(const glm::vec2 &offset)
+      {
+        textureOffset = offset;
+        return *this;
+      }
+
+      Circle& Circle::setTextureOffset(float x, float y)
+      {
+        textureOffset = glm::vec2(x, y);
+        return *this;
+      }
+
+      Circle& Circle::setTextureScale(float scale)
+      {
+        textureScale = scale;
+        return *this;
+      }
+
       Circle& Circle::setColor(const glm::vec4 &color)
       {
         this->color = color;
@@ -43,6 +61,8 @@ namespace chr
         return *this;
       }
 
+      // ---
+
       template <>
       void Circle::append(IndexedVertexBatch<XYZ, GLushort> &batch, Matrix &matrix, float x, float y) const
       {
@@ -53,11 +73,43 @@ namespace chr
 
         for (int i = 0; i < n; i++)
         {
-          float d = fmin(aa, i * segmentLength / r);
-          float xx = x + sinf(a1 + d) * r;
-          float yy = y + cosf(a1 + d) * r;
+          float dd = a1 + fmin(aa, i * segmentLength / r);
+          float xx = +sinf(dd) * r;
+          float yy = +cosf(dd) * r;
 
-          batch.addVertex(matrix.transformPoint(xx, yy));
+          batch.addVertex(matrix.transformPoint(x + xx, y + yy));
+
+          if (i < n - 1)
+          {
+            if (frontFace == GL_CW)
+            {
+              batch.addIndices(i + 1, 0, i + 2);
+            }
+            else
+            {
+              batch.addIndices(0, i + 1, i + 2);
+            }
+          }
+        }
+
+        batch.incrementIndices(n + 1);
+      }
+
+      template <>
+      void Circle::append(IndexedVertexBatch<XYZ.UV, GLushort> &batch, Matrix &matrix, float x, float y) const
+      {
+        float aa = fabsf(a2 - a1);
+        int n = ceilf(aa * r / segmentLength) + 1;
+
+        batch.addVertex(matrix.transformPoint(x, y), getTextureCoords(batch.texture, 0, 0));
+
+        for (int i = 0; i < n; i++)
+        {
+          float dd = a1 + fmin(aa, i * segmentLength / r);
+          float xx = +sinf(dd) * r;
+          float yy = +cosf(dd) * r;
+
+          batch.addVertex(matrix.transformPoint(x + xx, y + yy), getTextureCoords(batch.texture, xx, yy));
 
           if (i < n - 1)
           {
@@ -85,11 +137,11 @@ namespace chr
 
         for (int i = 0; i < n; i++)
         {
-          float d = fmin(aa, i * segmentLength / r);
-          float xx = x + sinf(a1 + d) * r;
-          float yy = y + cosf(a1 + d) * r;
+          float dd = a1 + fmin(aa, i * segmentLength / r);
+          float xx = +sinf(dd) * r;
+          float yy = +cosf(dd) * r;
 
-          batch.addVertex(matrix.transformPoint(xx, yy), color);
+          batch.addVertex(matrix.transformPoint(x + xx, y + yy), color);
 
           if (i < n - 1)
           {
@@ -105,6 +157,45 @@ namespace chr
         }
 
         batch.incrementIndices(n + 1);
+      }
+
+      template <>
+      void Circle::append(IndexedVertexBatch<XYZ.UV.RGBA, GLushort> &batch, Matrix &matrix, float x, float y) const
+      {
+        float aa = fabsf(a2 - a1);
+        int n = ceilf(aa * r / segmentLength) + 1;
+
+        batch.addVertex(matrix.transformPoint(x, y), getTextureCoords(batch.texture, 0, 0), color);
+
+        for (int i = 0; i < n; i++)
+        {
+          float dd = a1 + fmin(aa, i * segmentLength / r);
+          float xx = +sinf(dd) * r;
+          float yy = +cosf(dd) * r;
+
+          batch.addVertex(matrix.transformPoint(x + xx, y + yy), getTextureCoords(batch.texture, xx, yy), color);
+
+          if (i < n - 1)
+          {
+            if (frontFace == GL_CW)
+            {
+              batch.addIndices(i + 1, 0, i + 2);
+            }
+            else
+            {
+              batch.addIndices(0, i + 1, i + 2);
+            }
+          }
+        }
+
+        batch.incrementIndices(n + 1);
+      }
+
+      // ---
+
+      glm::vec2 Circle::getTextureCoords(const Texture &texture, float x, float y) const
+      {
+        return (glm::vec2(x, y) - textureOffset) / (texture.innerSize * textureScale);
       }
     }
   }
--- core/src/gl/draw/Circle.h
+++ core/src/gl/draw/Circle.h
@@ -15,6 +15,10 @@ namespace chr
       public:
         Circle& setFrontFace(GLenum mode);
 
+        Circle& setTextureOffset(const glm::vec2 &offset);
+        Circle& setTextureOffset(float x, float y);
+        Circle& setTextureScale(float scale);
+
         Circle& setColor(const glm::vec4 &color);
         Circle& setColor(float r, float g, float b, float a);
 
@@ -27,12 +31,16 @@ namespace chr
 
       protected:
         GLenum frontFace = GL_CCW;
-        glm::vec4 color;
+        glm::vec2 textureOffset;
+        float textureScale = 1;
+        glm::vec4 color = { 1, 1, 1, 1 };
 
         float r = 1;
         float a1 = 0;
         float a2 = TWO_PI;
         float segmentLength = 8;
+
+        glm::vec2 getTextureCoords(const gl::Texture &texture, float x, float y) const;
       };
     }
   }
