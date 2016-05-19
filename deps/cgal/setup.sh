772221a3a09e6abbb5cefe9a602b1cb444334ed7
Ariel Malka <ariel@chronotext.org>
2016-05-19 12:47:47 +0300

    CGAL: ADDING setup.sh AND clean.sh

---
+++ deps/cgal/setup.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+ARCHIVE_ZIP="master.zip"
+ARCHIVE_SRC="https://github.com/arielm/cgal/archive//$ARCHIVE_ZIP"
+ARCHIVE_DIR="cgal-master"
+
+# ---
+
+rm -rf build
+mkdir -p build
+cd build
+
+if [ ! -f $ARCHIVE_ZIP ]; then
+  echo "DOWNLOADING $ARCHIVE_SRC"
+  curl -L -O $ARCHIVE_SRC
+
+  if [ $? != 0 ] || [ ! -f $ARCHIVE_ZIP ]; then
+    echo "DOWNLOADING FAILED!"
+    exit 1
+  fi
+fi
+
+# ---
+
+echo "UNPACKING $ARCHIVE_ZIP..."
+unzip -q $ARCHIVE_ZIP
+
+if [ $? != 0 ] || [ ! -d $ARCHIVE_DIR ]; then
+  echo "UNPACKING FAILED!"
+  exit 1
+fi
+
+rm $ARCHIVE_ZIP
+mv $ARCHIVE_DIR src
