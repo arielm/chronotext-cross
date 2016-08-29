b921db4b1f9ded6b414d2fbc91791d1d0b9ddb46
Ariel Malka <ariel@chronotext.org>
2016-08-29 19:42:12 +0300

    cross/WindowInfo: USING math::Rectf INSTEAD OF glm::vec4 FOR BOUNDS

--- core/src/cross/WindowInfo.h
+++ core/src/cross/WindowInfo.h
@@ -1,79 +1,71 @@
-/*
- * THE NEW CHRONOTEXT TOOLKIT: https://github.com/arielm/new-chronotext-toolkit
- * COPYRIGHT (C) 2012-2015, ARIEL MALKA ALL RIGHTS RESERVED.
- *
- * THE FOLLOWING SOURCE-CODE IS DISTRIBUTED UNDER THE SIMPLIFIED BSD LICENSE:
- * https://github.com/arielm/new-chronotext-toolkit/blob/master/LICENSE.md
- */
-
 #pragma once
 
-#include "glm.h"
+#include "math/Rect.h"
 
 #include <iostream>
 
 namespace chr
 {
-    struct WindowInfo
+  struct WindowInfo
+  {
+    union
     {
-        union
-        {
-          glm::vec2 size;
-          struct { float width, height; };
-        };
-
-        int aaSamples;
-        int depthBits;
-
-        static WindowInfo create(int width, int height, int aaSamples = 0, int depthBits = 0)
-        {
-            return WindowInfo(width, height, aaSamples, depthBits);
-        }
-        
-        WindowInfo(const glm::ivec2 &size = glm::zero<glm::ivec2>(), int aaSamples = 0, int depthBits = 0)
-        :
-        size(size),
-        aaSamples(aaSamples),
-        depthBits(depthBits)
-        {}
-        
-        WindowInfo(int width, int height, int aaSamples = 0, int depthBits = 0)
-        :
-        size(width, height),
-        aaSamples(aaSamples),
-        depthBits(depthBits)
-        {}
-        
-        void rotate()
-        {
-            size = size.yx();
-        }
-        
-        glm::vec4 bounds() const
-        {
-            return glm::vec4(0, 0, width, height);
-        }
-
-        glm::vec2 center() const
-        {
-            return size * 0.5f;
-        }
-        
-        float aspectRatio() const
-        {
-            return (size.y > 0) ? (width / height) : 0;
-        }
-        
-        friend std::ostream& operator<<(std::ostream &lhs, const WindowInfo &rhs)
-        {
-            lhs
-            << "{"
-            << "size: [" << rhs.width << ", " << rhs.height << "]"
-            << ", anti-aliasing samples: " << rhs.aaSamples
-            << ", depth-bits: " << rhs.depthBits
-            << "}";
-            
-            return lhs;
-        }
+      glm::vec2 size;
+      struct { float width, height; };
     };
+
+    int aaSamples;
+    int depthBits;
+
+    static WindowInfo create(int width, int height, int aaSamples = 0, int depthBits = 0)
+    {
+      return WindowInfo(width, height, aaSamples, depthBits);
+    }
+    
+    WindowInfo(const glm::ivec2 &size = glm::zero<glm::ivec2>(), int aaSamples = 0, int depthBits = 0)
+    :
+    size(size),
+    aaSamples(aaSamples),
+    depthBits(depthBits)
+    {}
+    
+    WindowInfo(int width, int height, int aaSamples = 0, int depthBits = 0)
+    :
+    size(width, height),
+    aaSamples(aaSamples),
+    depthBits(depthBits)
+    {}
+    
+    void rotate()
+    {
+      size = size.yx();
+    }
+    
+    math::Rectf bounds() const
+    {
+      return math::Rectf(0, 0, width, height);
+    }
+
+    glm::vec2 center() const
+    {
+      return size * 0.5f;
+    }
+    
+    float aspectRatio() const
+    {
+      return (size.y > 0) ? (width / height) : 0;
+    }
+    
+    friend std::ostream& operator<<(std::ostream &lhs, const WindowInfo &rhs)
+    {
+      lhs
+      << "{"
+      << "size: [" << rhs.width << ", " << rhs.height << "]"
+      << ", anti-aliasing samples: " << rhs.aaSamples
+      << ", depth-bits: " << rhs.depthBits
+      << "}";
+      
+      return lhs;
+    }
+  };
 }
