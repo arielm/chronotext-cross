bbc751ccd19c2a1023dea6220b90f34c9f256b56
Ariel Malka <ariel@chronotext.org>
2016-09-02 01:46:53 +0300

    CLEANUP

--- tests/TestingCGAL/src/cgal.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <CGAL/Polyhedron_3.h>
-#include <CGAL/Nef_polyhedron_3.h>
-#include <CGAL/Exact_predicates_exact_constructions_kernel.h>
-
-typedef CGAL::Exact_predicates_exact_constructions_kernel Kernel;
-typedef CGAL::Nef_polyhedron_3<Kernel> Nef_polyhedron;
