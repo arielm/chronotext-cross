e56ab2ed28cb89badded87aaaeecd28349cb3c4c
Ariel Malka <ariel@chronotext.org>
2016-12-04 01:47:53 +0200

    TestingIO: READ / WRITE TEST
    FIXME: DOESN'T WORK AS INTENDED ON iOS
    REQUIRED: XCODE PROJECT, IN ORDER TO DEBUG PROPERLY

--- tests/TestingIO/src/Sketch.cpp
+++ tests/TestingIO/src/Sketch.cpp
@@ -1,14 +1,25 @@
 #include "Sketch.h"
 
+#include <google/protobuf/io/coded_stream.h>
+#include <google/protobuf/io/zero_copy_stream_impl.h>
+
+#include <fcntl.h> // FOR MINGW + O_RDONLY
+
 using namespace std;
 using namespace chr;
 using namespace gl;
+using namespace google::protobuf;
 
 void Sketch::setup()
 {
-  LOGI << systemManager().getDocumentsFolder() << endl;
+  auto documentsFolder = systemManager().getDocumentsFolder();
+  LOGI << documentsFolder << endl;
+
+  writeFile(documentsFolder / "test.dat");
+  readFile(documentsFolder / "test.dat");
 
   // ---
+
   glDisable(GL_DEPTH_TEST);
   glDepthMask(GL_FALSE);
 
@@ -18,6 +29,79 @@ void Sketch::setup()
 
 void Sketch::draw()
 {
-  glClearColor(0.5f, 0.5f, 0.5f, 1);
+  if (success)
+  {
+    glClearColor(1, 0, 0, 1);
+  }
+  else
+  {
+    glClearColor(0.5f, 0.5f, 0.5f, 1);
+  }
+
   glClear(GL_COLOR_BUFFER_BIT);
+}
+
+void Sketch::writeFile(const fs::path &filePath)
+{
+  int fd = open(filePath.string().data(), O_RDWR | O_CREAT | O_TRUNC);
+
+  if (fd > 0)
+  {
+    auto rawOutput = new io::FileOutputStream(fd);
+    auto codedOutput = new io::CodedOutputStream(rawOutput);
+
+    // ---
+
+    string text = "Which way to the station?";
+    codedOutput->WriteVarint32(text.size());
+    codedOutput->WriteRaw(text.data(), text.size());
+
+    codedOutput->WriteLittleEndian32(123456789);
+    codedOutput->WriteLittleEndian32(encodeFloat(123.456f));
+
+    // ---
+
+    delete codedOutput;
+    delete rawOutput;
+    close(fd);
+  }
+}
+
+void Sketch::readFile(const fs::path &filePath)
+{
+  int fd = open(filePath.string().data(), O_RDONLY);
+
+  if (fd > 0)
+  {
+    auto rawInput = new io::FileInputStream(fd);
+    auto codedInput = new io::CodedInputStream(rawInput);
+
+    // ---
+
+    uint32 size;
+    codedInput->ReadVarint32(&size);
+
+    std::string text;
+    text.resize(size);
+
+    codedInput->ReadRaw(&text[0], size);
+
+    //
+
+    uint32_t i;
+    codedInput->ReadLittleEndian32(&i);
+
+    float f;
+    codedInput->ReadLittleEndian32(reinterpret_cast<uint32_t*>(&f));
+
+    //
+
+    success = (text == "Which way to the station?") && (i = 123456789) && (f == 123.456f);
+
+    // ---
+
+    delete codedInput;
+    delete rawInput;
+    close(fd);
+  }
 }
--- tests/TestingIO/src/Sketch.h
+++ tests/TestingIO/src/Sketch.h
@@ -9,4 +9,24 @@ public:
 
   void setup() final;
   void draw() final;
+
+protected:
+  bool success = false;
+
+  void writeFile(const fs::path &filePath);
+  void readFile(const fs::path &filePath);
+
+  static uint32_t encodeFloat(float value)
+  {
+    union {float f; uint32_t i;};
+    f = value;
+    return i;
+  }
+
+  static uint64_t encodeDouble(double value)
+  {
+    union {double f; uint64_t i;};
+    f = value;
+    return i;
+  }
 };
