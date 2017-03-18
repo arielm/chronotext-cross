fa8fc6d76efe55814ed37691e909d8d04c3ba9eb
Ariel Malka <ariel@chronotext.org>
2017-03-18 17:13:28 +0200

    EMSCRIPTEN: FIXING KEY-MAPPINGS

--- core/src/chr/emscripten/Keyboard.h
+++ core/src/chr/emscripten/Keyboard.h
@@ -7,6 +7,16 @@ namespace chr
 {
   static const std::map<int, int> KEYMAP
   {
+    { 33, KEY_PAGE_UP },
+    { 34, KEY_PAGE_DOWN },
+    { 35, KEY_END },
+    { 36, KEY_HOME },
+    { 37, KEY_LEFT },
+    { 38, KEY_UP },
+    { 39, KEY_RIGHT },
+    { 40, KEY_DOWN },
+    { 46, KEY_DELETE },
+
     { 0x10, KEY_SHIFT },
     { 0x11, KEY_CTRL },
     { 0x12, KEY_ALT },
