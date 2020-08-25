1f4759c072ac4bf9e0906af6bd317381167481cb
Ariel Malka <ariel@chronotext.org>
2020-08-25 13:27:13 +0300

    Adapting deps to RPI

---
+++ deps/jsoncpp/build.rpi.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+PLATFORM="rpi"
+
+TREE_DIR="../../tree/jsoncpp"
+SRC_DIR="$TREE_DIR/src"
+BUILD_DIR="build/$PLATFORM"
+INSTALL_DIR="tmp/$PLATFORM"
+
+SRC_PATH="$(pwd)/$SRC_DIR"
+INSTALL_PATH="$(pwd)/$INSTALL_DIR"
+
+if [ ! -d "$SRC_PATH" ]; then
+  echo "SOURCE NOT FOUND!"
+  exit 1
+fi
+
+# ---
+
+TOOLCHAIN_FILE="$CROSS_PATH/core/cmake/toolchains/rpi.cmake"
+
+cmake -H"$SRC_DIR" -B"$BUILD_DIR" \
+  -DCMAKE_TOOLCHAIN_FILE="$TOOLCHAIN_FILE" -G Ninja \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DLIBRARY_OUTPUT_PATH="$INSTALL_PATH/lib" \
+  -DJSONCPP_WITH_TESTS=OFF -DJSONCPP_WITH_POST_BUILD_UNITTEST=OFF -DJSONCPP_WITH_PKGCONFIG_SUPPORT=OFF
+
+if [ $? != 0 ]; then
+  echo "CONFIGURATION FAILED!"
+  exit -1
+fi
+
+# ---
+
+rm -rf "$INSTALL_PATH"
+cmake --build "$BUILD_DIR"
+
+if [ $? != 0 ]; then
+  echo "BUILD FAILED!"
+  exit -1
+fi
+
+rm -rf   "$TREE_DIR/$PLATFORM/lib"
+mkdir -p "$TREE_DIR/$PLATFORM/lib"
+mv "tmp/$PLATFORM/lib" "$TREE_DIR/$PLATFORM"
+
+cd "$TREE_DIR/$PLATFORM"
+ln -s "../src/include"
---
+++ deps/protobuf/build.rpi.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+PLATFORM="rpi"
+
+TREE_DIR="../../tree/protobuf"
+SRC_DIR="$TREE_DIR/src"
+BUILD_DIR="build/$PLATFORM"
+INSTALL_DIR="tmp/$PLATFORM"
+
+SRC_PATH="$(pwd)/$SRC_DIR"
+INSTALL_PATH="$(pwd)/$INSTALL_DIR"
+
+if [ ! -d "$SRC_PATH" ]; then
+  echo "SOURCE NOT FOUND!"
+  exit 1
+fi
+
+# ---
+
+TOOLCHAIN_FILE="$CROSS_PATH/core/cmake/toolchains/rpi.cmake"
+
+cmake -H"$SRC_DIR/cmake" -B"$BUILD_DIR" \
+  -DCMAKE_TOOLCHAIN_FILE="$TOOLCHAIN_FILE" -G Ninja \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DLIBRARY_OUTPUT_PATH="$INSTALL_PATH/lib" -DEXECUTABLE_OUTPUT_PATH="$INSTALL_PATH" \
+  -DBUILD_PROTOC=ON -DZLIB=ON
+
+if [ $? != 0 ]; then
+  echo "CONFIGURATION FAILED!"
+  exit 1
+fi
+
+# ---
+
+rm -rf "$INSTALL_PATH"
+cmake --build "$BUILD_DIR"
+
+if [ $? != 0 ]; then
+  echo "BUILD FAILED!"
+  exit 1
+fi
+
+rm -rf   "$TREE_DIR/$PLATFORM"
+mkdir -p "$TREE_DIR/$PLATFORM"
+mv "tmp/$PLATFORM" "$TREE_DIR"
+
+cd "$TREE_DIR/$PLATFORM"
+ln -s "../src/src" include
