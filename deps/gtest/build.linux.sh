4a660267b3b83d5d3cf020a5e44af5bf4b517026
Ariel Malka <ariel@chronotext.org>
2020-09-10 16:38:38 +0300

    Updating deps to linux

---
+++ deps/gtest/build.linux.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+PLATFORM="linux"
+
+TREE_DIR="../../tree/gtest"
+SRC_DIR="$TREE_DIR/src"
+BUILD_DIR="build/$PLATFORM"
+INSTALL_DIR="tmp/$PLATFORM"
+
+SRC_PATH="$(pwd)/$SRC_DIR"
+INSTALL_PATH="$(pwd)/$INSTALL_DIR"
+
+if [ ! -d "$SRC_PATH" ]; then
+  echo "SOURCE NOT FOUND!"
+  exit 1
+fi
+
+# ---
+
+TOOLCHAIN_FILE="$CROSS_PATH/core/cmake/toolchains/linux.cmake"
+
+cmake -H"$SRC_DIR" -B"$BUILD_DIR" \
+  -DCMAKE_TOOLCHAIN_FILE="$TOOLCHAIN_FILE" -G Ninja \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DLIBRARY_OUTPUT_PATH="$INSTALL_PATH/lib"
+
+if [ $? != 0 ]; then
+  echo "CONFIGURATION FAILED!"
+  exit -1
+fi
+
+# ---
+
+rm -rf "$INSTALL_PATH"
+cmake --build "$BUILD_DIR"
+
+if [ $? != 0 ]; then
+  echo "BUILD FAILED!"
+  exit -1
+fi
+
+rm -rf   "$TREE_DIR/$PLATFORM/lib"
+mkdir -p "$TREE_DIR/$PLATFORM/lib"
+mv "tmp/$PLATFORM/lib" "$TREE_DIR/$PLATFORM"
+
+cd "$TREE_DIR/$PLATFORM"
+ln -s "../src/include"
