7924f35167e23c435b9d88683ed5a69574fa77dd
Ariel Malka <ariel@chronotext.org>
2022-10-08 18:50:09 +0300

    TestingDrawing3d: Optimization

--- tests/TestingDrawing3d/src/Sketch.h
+++ tests/TestingDrawing3d/src/Sketch.h
@@ -15,9 +15,9 @@ protected:
   chr::gl::shaders::LambertShader lambertShader;
   chr::gl::shaders::ColorShader colorShader;
 
-  chr::gl::IndexedVertexBatch<chr::gl::XYZ.N.UV> sunBatch;
-  chr::gl::IndexedVertexBatch<chr::gl::XYZ.N.UV> earthBatch;
-  chr::gl::IndexedVertexBatch<chr::gl::XYZ.N.UV> moonBatch;
+  chr::gl::IndexedVertexBatch<chr::gl::XYZ.N> sunBatch;
+  chr::gl::IndexedVertexBatch<chr::gl::XYZ.N> earthBatch;
+  chr::gl::IndexedVertexBatch<chr::gl::XYZ.N> moonBatch;
   chr::gl::VertexBatch<chr::gl::XYZ> pathBatch;
 
   void createPath();
