e4ed7498b52794cd2da042b038f92e9646873594
Ariel Malka <ariel@chronotext.org>
2022-10-08 23:39:13 +0300

    TestingDrawing3d: Cosmetic tweak

--- tests/TestingDrawing3d/src/Sketch.cpp
+++ tests/TestingDrawing3d/src/Sketch.cpp
@@ -42,6 +42,7 @@ void Sketch::setup()
 
   glEnable(GL_CULL_FACE);
   glEnable(GL_DEPTH_TEST);
+  glDepthMask(GL_TRUE);
 
   glEnable(GL_BLEND);
   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
