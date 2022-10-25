2c4e93b293bfc23fd78ac5c202d442f77aea7af7
Ariel Malka <ariel@chronotext.org>
2022-10-25 20:29:32 +0300

    core: Including stb library properly

--- core/src/chr/cross/CrossSketch.cpp
+++ core/src/chr/cross/CrossSketch.cpp
@@ -5,8 +5,12 @@
 #endif
 
 #if defined(CHR_PLATFORM_DESKTOP)
+  #define STB_IMAGE_IMPLEMENTATION
   #define STB_IMAGE_WRITE_IMPLEMENTATION
+  #define STB_IMAGE_RESIZE_IMPLEMENTATION
+  #include "stb_image.h"
   #include "stb_image_write.h"
+  #include "stb_image_resize.h"
 #endif
 
 #include <iomanip>
